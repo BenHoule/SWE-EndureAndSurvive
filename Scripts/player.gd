b5a93eedad4c9d79c7c58f41ac966795513da73e
Cam Gagne <gagne.cam.17@gmail.com>
2025-11-18 18:46:24 -0500

    Created level 1 UI and player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -22,5 +22,8 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.play("right")
 	else:
 		animated_sprite_2d.play("default")
+		
+	if Input.is_action_just_pressed("next_level"):
+		get_tree().change_scene_to_file("res://Scenes/Shelter-Building-Lvl.tscn")
 
 	move_and_slide()
