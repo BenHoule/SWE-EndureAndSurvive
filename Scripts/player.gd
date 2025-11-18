f92809cd6ded4aaec18b65548f3fcfc6c4a16a9e
Cam Gagne <gagne.cam.17@gmail.com>
2025-11-17 20:44:34 -0500

    Basic Player movement and Level creation.

---
+++ Scripts/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+
+	var direction2 := Input.get_axis("ui_up", "ui_down")
+	if direction2:
+		velocity.y = direction2 * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
